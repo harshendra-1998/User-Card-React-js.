7fc3cc8611aacbce775e0071f0b44b2ac0ca3a79
harshendra-1998 <harshpraja66666@gmail.com>
2021-02-23 18:50:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-qtfuer
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-qtfuer)
---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./style.css";
+import axios from "axios";
+
+export default function App() {
+  const [data, setdata] = useState([]);
+  const [count, setcount] = useState(0);
+  if (count < 1) {
+    axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+      setdata([...data, ...res.data]);
+    });
+    setcount(1);
+  }
+
+  return (
+    <div>
+      {data.map(datab => (
+        <div
+          className="hello"
+          key={datab.id}
+          style={{
+            display: "flex",
+            background: "white",
+            margin: "5px"
+          }}
+        >
+          <section style={{ width: "70%", position: "relative" }}>
+            <p>Name : {datab.name}</p>
+            <p>E-mail : {datab.email}</p>
+            <p>Phone No. : {datab.phone}</p>
+            <p>
+              Address : {datab.address.street}, {datab.address.suite},{" "}
+              {datab.address.city}, {datab.address.zipcode}{" "}
+            </p>
+            <p>
+              Website : <a href={datab.website}>{datab.website}</a>
+            </p>
+            <p>Company : {datab.company.name}</p>
+          </section>
+          <section
+            style={{
+              width: "30%",
+              position: "relative"
+            }}
+          >
+            <img
+              style={{
+                width: "90%",
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                margin: "10px"
+              }}
+              src="https://www.w3schools.com/howto/img_avatar.png"
+            />
+          </section>
+        </div>
+      ))}
+    </div>
+  );
+}
